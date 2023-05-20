8cec120939d2a3a61c82c93a8a31c730f3561602
typ2-f <myg.min3btn@gmail.com>
2023-05-21 05:41:46 +0900

    [add] URL仕様書の追加 ルートの追加 付随するコントローラー・モデルの作成

---
+++ app/Models/StorageBookLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StorageBookLog extends Model
+{
+    use HasFactory;
+}
--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,23 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+//Route::get('/',[IndexController::class,'index']);
+
 Route::middleware('auth')->group(function () {
+    //マイページ
+    Route::get('/home', [UserController::class, 'index']);
+
+    //書籍に関するリソースルート
+    Route::resource('/books', BookController::class);
+
+    //書庫
+    Route::get('/storages', [StorageController::class, 'index']);
+    Route::get('/storages/{id}', [BookInStorageController::class, 'index']);
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
